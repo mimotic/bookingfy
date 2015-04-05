aa07ae8472a8a3280df7207bb9c2b7d1ccc14966
miktown <mcacho@mimotic.com>
2015-04-06 00:51:29 +0200

    sql generator

---
+++ bookingfy.sql
@@ -0,0 +1,183 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.7
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 05-04-2015 a las 21:49:37
+-- Versión del servidor: 5.6.20
+-- Versión de PHP: 5.5.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `bookingfy`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `deporte`
+--
+
+CREATE TABLE IF NOT EXISTS `deporte` (
+`id` int(6) NOT NULL,
+  `nombre` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `horarios`
+--
+
+CREATE TABLE IF NOT EXISTS `horarios` (
+`id` int(6) NOT NULL,
+  `inicio` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pistas`
+--
+
+CREATE TABLE IF NOT EXISTS `pistas` (
+`id` int(6) NOT NULL,
+  `id_deporte` int(6) NOT NULL,
+  `precio_pista` decimal(10,0) NOT NULL,
+  `precio_luz` decimal(10,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reservas`
+--
+
+CREATE TABLE IF NOT EXISTS `reservas` (
+`id` int(6) NOT NULL,
+  `id_usuario` int(255) NOT NULL,
+  `id_pista` int(255) NOT NULL,
+  `id_hora` int(255) NOT NULL,
+  `fecha_pista` int(255) NOT NULL,
+  `luz` int(1) NOT NULL,
+  `fecha_reserva` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE IF NOT EXISTS `usuarios` (
+`id` int(6) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellidos` varchar(100) NOT NULL,
+  `expediente` varchar(50) DEFAULT NULL,
+  `dni` varchar(50) NOT NULL,
+  `mail` varchar(50) NOT NULL,
+  `password` int(50) NOT NULL,
+  `rol` int(1) NOT NULL DEFAULT '0',
+  `fecha_alta` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `deporte`
+--
+ALTER TABLE `deporte`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `horarios`
+--
+ALTER TABLE `horarios`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `pistas`
+--
+ALTER TABLE `pistas`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `reservas`
+--
+ALTER TABLE `reservas`
+ ADD PRIMARY KEY (`id`),
+ ADD UNIQUE KEY (`id_pista`,`id_hora`,`fecha_pista`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+ ADD PRIMARY KEY (`id`),
+ ADD UNIQUE KEY `expediente`,
+ ADD UNIQUE KEY `dni`,
+ ADD UNIQUE KEY `mail`;
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `deporte`
+--
+ALTER TABLE `deporte`
+MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `horarios`
+--
+ALTER TABLE `horarios`
+MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `pistas`
+--
+ALTER TABLE `pistas`
+MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `reservas`
+--
+ALTER TABLE `reservas`
+MODIFY `id` int(6) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10210;
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
+
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `reservas`
+--
+ALTER TABLE `reservas`
+ADD CONSTRAINT `FK_4FA1275DB47F08D` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`),
+ADD CONSTRAINT `FK_CD55B27DB47F08D` FOREIGN KEY (`id_pista`) REFERENCES `pistas` (`id`),
+ADD CONSTRAINT `FK_5E0E26E8B47F08D` FOREIGN KEY (`id_hora`) REFERENCES `horarios` (`id`);
+
+
+--
+-- Filtros para la tabla `pistas`
+--
+ALTER TABLE `pistas`
+ADD CONSTRAINT `FK_2227D407B47F08D` FOREIGN KEY (`id_deporte`) REFERENCES `deporte` (`id`);
+
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
