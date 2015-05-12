3f1fbcfbbbddeac7ac2d3bfee07f7311cb5599bd
miktown <mcacho@mimotic.com>
2015-05-12 22:55:56 +0200

    cambio de estructura del proyecto

---
+++ src/js/views/perfil.js
@@ -0,0 +1,52 @@
+var Backbone   = require('backbone'),
+    Handlebars = require('handlebars'),
+    $          = require('jquery'),
+    Plantilla  = require('../partials/plantilla_perfil'),
+    Sesion     = require('../models/sesion');
+    // app        = Backbone.app;
+
+module.exports = Backbone.View.extend({
+
+  el: $('#perfil'),
+
+  template: Handlebars.compile(Plantilla.perfil),
+
+  events: {
+    'click #logotipoLink': 'goHome',
+    'click #logout': 'logout',
+    'click #user-welcome': 'goUserResume'
+  },
+
+  initialize: function () {
+    this.render();
+  },
+
+  resetear: function () {
+    this.$el.empty();
+  },
+
+  getUserData: function (){
+    var sesion = Sesion.getInstance();
+    var usuario = {};
+
+    if (sesion.get('mail') && sesion.get('apellidos')){
+      usuario.nombre = sesion.get('nombre');
+      usuario.apellidos = sesion.get('apellidos');
+      usuario.dni = sesion.get('dni');
+      usuario.expediente = sesion.get('expediente');
+      usuario.mail = sesion.get('mail');
+      usuario.apellidos = sesion.get('apellidos');
+      usuario.password = '**********';
+    }
+
+    return usuario;
+  },
+
+  render: function () {
+    var usuario = this.getUserData();
+    var html = this.template(usuario);
+    this.$el.html(html);
+    return this;
+  },
+
+});
