7b553f1659bbe3fff8fc12e82323e1b45cc55144
miktown <mcacho@mimotic.com>
2015-05-29 04:13:08 +0200

    stilos

--- src/js/partials/plantilla_add_deporte.js
+++ src/js/partials/plantilla_add_deporte.js
@@ -1,9 +1,9 @@
 var plantillas = plantillas || {};
 
 plantillas.add_deporte = '<div><h2 class="text-center">Añadir deporte</h2>'
-			+ '<input id="nombreDeporte" data-validado="" type="text" name="" value="">'
-			+'<div class="modal-botones"><a id="anadirDeporte" href="#" class="confirm-modal"><i class="demo-icon icon-user">&#xe853;</i> Añadir deporte</a>'
-			+'<a id="cerrarModal" class="cancel-modal supercerrar" href="#"><i class="demo-icon icon-user">&#xe804;</i> Cerrar</a><div>'
+			+ '<input id="nombreDeporte" class="inputDeporteSty" data-validado="" type="text" name="" value="" placeholder="Nombre Deporte">'
+			+'<div class="modal-botones"><a id="anadirDeporte" href="#" class="confirm-modal"><i class="demo-icon icon-user">&#xe81b;</i> Añadir deporte</a>'
+			+'<a id="cerrarModal" class="cancel-modal" href="#"><i class="demo-icon icon-user">&#xe804;</i> Cancelar</a><div>'
 			+'</div>';
 
 module.exports = plantillas;
